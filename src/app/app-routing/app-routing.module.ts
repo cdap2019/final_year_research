0dafa8ead0fbc3406f9051ef4012faea2637d0c8
Supun Dileepa <dilee1st.1995@gmail.com>
2019-06-26 05:47:02 +0530

    add land info link changed

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddLandInfoComponent } from '../add-land-info/add-land-info.component';
+import { MainComponent } from '../main/main.component';
 
 const appRoutes: Routes = [
   {
     path: 'addLand',
     component: AddLandInfoComponent
+  },
+  {
+    path: '',
+    component: MainComponent
   }
 ];
 
